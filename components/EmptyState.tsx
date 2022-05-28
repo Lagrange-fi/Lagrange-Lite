02db113df2c1d001001e1363e1adabcf860749f6
better-think <chaojin752@gmail.com>
2022-05-28 22:39:03 +0300

    add: trade page for pro versioin

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,5 +1,4 @@
 import { FunctionComponent, ReactNode } from 'react'
-import useMangoStore from '../stores/useMangoStore'
 import Button from './Button'
 
 interface EmptyStateProps {
@@ -8,6 +7,7 @@ interface EmptyStateProps {
   onClickButton?: () => void
   desc?: string
   title: string
+  disabled?: boolean
 }
 
 const EmptyState: FunctionComponent<EmptyStateProps> = ({
@@ -16,14 +16,12 @@ const EmptyState: FunctionComponent<EmptyStateProps> = ({
   onClickButton,
   desc,
   title,
+  disabled = false,
 }) => {
-  const wallet = useMangoStore((s) => s.wallet.current)
-  const mangoGroup = useMangoStore((s) => s.selectedMangoGroup.current)
-
   return (
-    <div className="flex flex-col items-center text-th-fgd-1 px-4 pb-2 rounded-lg">
-      <div className="w-6 h-6 mb-1 text-th-primary">{icon}</div>
-      <h2 className="text-base mb-1">{title}</h2>
+    <div className="when-disconnected flex flex-col items-center rounded-lg px-4 pb-2 text-th-fgd-1">
+      <div className="mb-1 h-6 w-6 text-th-primary">{icon}</div>
+      <h2 className="mb-1 text-base">{title}</h2>
       {desc ? (
         <p
           className={`text-center ${
@@ -34,11 +32,7 @@ const EmptyState: FunctionComponent<EmptyStateProps> = ({
         </p>
       ) : null}
       {buttonText && onClickButton ? (
-        <Button
-          className="mt-2"
-          onClick={onClickButton}
-          disabled={!wallet || !mangoGroup}
-        >
+        <Button className="mt-2" onClick={onClickButton} disabled={disabled}>
           {buttonText}
         </Button>
       ) : null}
